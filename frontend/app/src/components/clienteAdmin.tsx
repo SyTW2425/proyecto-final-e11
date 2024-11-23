d8ce74606ff3452c735ea7471660a987ce29eef0
Ithaisa <alu0101482194@ull.edu.es>
2024-11-23 19:58:34 +0000

    Formularios Cliente

--- frontend/app/src/components/clienteAdmin.tsx
+++ frontend/app/src/components/clienteAdmin.tsx
@@ -14,7 +14,10 @@ import axios from 'axios';
 
 const ClienteAdmin: React.FC = () => {
   const [clientes, setClientes] = useState<any[]>([]);
-  const [mostrarFormulario, setMostrarFormulario] = useState(false); // Estado para controlar la visibilidad del formulario
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [mostrarFormularioEliminar, setMostrarFormularioEliminar] = useState(false); 
+  const [dniEliminar, setDniEliminar] = useState<string>(''); // Estado para almacenar el DNI
+  // Estado para controlar la visibilidad del formulario
   const [nuevoCliente, setNuevoCliente] = useState({
     id_: '',
     nombre_: '',
@@ -24,10 +27,10 @@ const ClienteAdmin: React.FC = () => {
   });
   //const [dniAEliminar, setDniAEliminar] = useState<string>(''); // Estado para almacenar el DNI a eliminar
 
- 
+
   useEffect(() => {
     // Consumir API
-    fetch('http://localhost:5000/clientes') 
+    fetch('http://localhost:5000/clientes')
       .then((response) => response.json())
       .then((data) => setClientes(data))
       .catch((error) => console.error('Error al cargar los datos:', error));
@@ -55,7 +58,7 @@ const ClienteAdmin: React.FC = () => {
       alert('Por favor, rellena todos los campos');
       return;
     }
-    
+
     const nuevoCliente2 = {
       id_: nuevoCliente.id_,
       nombre_: nuevoCliente.nombre_,
@@ -66,22 +69,42 @@ const ClienteAdmin: React.FC = () => {
     alert(JSON.stringify(nuevoCliente2, null, 2))
     // Enviar el nuevo cliente al servidor como json
     axios.post('http://localhost:5000/clientes', nuevoCliente2)
-    .then((response) => {
-      // Añadir el nuevo cliente al estado
-      alert("Respuesta del backend:" + response.data);
-      setClientes((prevClientes) => [...prevClientes, response.data]);
-      alert('Cliente creado correctamente');
-      setMostrarFormulario(false); // Ocultar el formulario después de enviar
-    })
-    .catch((error) => {
-      alert('Hubo un error al crear el cliente');
-    });
-};
-
-  const handleCrearCliente = () => {
-    alert('Función para crear un nuevo cliente');
-    // Aquí puedes redirigir a un formulario para crear un cliente
+      .then((response) => {
+        // Añadir el nuevo cliente al estado
+        alert("Respuesta del backend:" + response.data);
+        setClientes((prevClientes) => [...prevClientes, response.data]);
+        alert('Cliente creado correctamente');
+        setMostrarFormulario(false); // Ocultar el formulario después de enviar
+      })
+      .catch((error) => {
+        alert('Hubo un error al crear el cliente');
+      });
   };
+
+  const manejarEnvioEliminarCliente = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!dniEliminar) {
+      alert('Por favor, ingresa un DNI válido.');
+      return;
+    }
+
+    axios
+      .delete(`http://localhost:5000/clientes/${dniEliminar}`)
+      .then(() => {
+        setClientes((prevClientes) =>
+          prevClientes.filter((cliente) => cliente.id_ !== dniEliminar)
+        );
+        alert('Cliente eliminado correctamente');
+        setMostrarFormularioEliminar(false); // Ocultar formulario tras la eliminación
+        setDniEliminar(''); // Limpiar el campo
+      })
+      .catch((error) => {
+        console.error('Error al eliminar el cliente:', error);
+        alert('Hubo un error al eliminar el cliente');
+      });
+  };
+
 
   const handleEditarCliente = () => {
     alert('Función para editar un cliente existente');
@@ -98,20 +121,20 @@ const ClienteAdmin: React.FC = () => {
     // Pedir al usuario el DNI del cliente que quiere eliminar
     const dniCliente = prompt('Introduce el DNI del cliente que deseas eliminar:');
     axios
-          .delete(`http://localhost:5000/clientes/${dniCliente}`)
-          .then(() => {
-            // Filtrar el cliente eliminado del estado
-            setClientes((prevClientes) =>
-              prevClientes.filter((cliente) => cliente.id_ !== dniCliente)
-            );
-            alert('Cliente eliminado correctamente');
-          })
-          .catch((error) => {
-            console.error('Error al eliminar el cliente:', error);
-            alert('Hubo un error al eliminar el cliente');
-          });
-    };
-  
+      .delete(`http://localhost:5000/clientes/${dniCliente}`)
+      .then(() => {
+        // Filtrar el cliente eliminado del estado
+        setClientes((prevClientes) =>
+          prevClientes.filter((cliente) => cliente.id_ !== dniCliente)
+        );
+        alert('Cliente eliminado correctamente');
+      })
+      .catch((error) => {
+        console.error('Error al eliminar el cliente:', error);
+        alert('Hubo un error al eliminar el cliente');
+      });
+  };
+
 
   return (
     <div className={styles.container}>
@@ -139,104 +162,136 @@ const ClienteAdmin: React.FC = () => {
             </div>
           </div>
         </nav>
-         {/* Sección de botones */}
-         <div className={styles.buttonContainer}>
-         <button className={styles.actionButton} onClick={() => setMostrarFormulario(!mostrarFormulario)}>Crear nuevo cliente</button>
+        {/* Sección de botones */}
+        <div className={styles.buttonContainer}>
+          <button className={styles.actionButton} onClick={() => setMostrarFormulario(!mostrarFormulario)}>Crear nuevo cliente</button>
           <button className={styles.actionButton} onClick={handleEditarCliente}>
             Editar cliente existente
           </button>
-          <button className={styles.actionButton} onClick={handleEliminarCliente}>
+          <button
+            className={styles.actionButton}
+            onClick={() => setMostrarFormularioEliminar(true)}
+          >
             Eliminar cliente
           </button>
-          
+
           <button className={styles.actionButton} onClick={handleBuscarCliente}>
             Buscar cliente
           </button>
         </div>
-        {mostrarFormulario && (
-  <form onSubmit={manejarEnvioFormulario} className={styles.formularioContainer}>
-    <h2 className={styles.formTitle}>Crear Nuevo Cliente</h2>
-    <div className={styles.formGroup}>
-      <label className={styles.formLabel}>DNI:</label>
-      <input
-        type="text"
-        name="id_"
-        value={nuevoCliente.id_}
-        onChange={manejarCambioFormulario}
-        className={styles.formInput}
-        required
-      />
-    </div>
-    <div className={styles.formGroup}>
-      <label className={styles.formLabel}>Nombre:</label>
-      <input
-        type="text"
-        name="nombre_"
-        value={nuevoCliente.nombre_}
-        onChange={manejarCambioFormulario}
-        className={styles.formInput}
-        required
-      />
-    </div>
-    <div className={styles.formGroup}>
-      <label className={styles.formLabel}>Contacto:</label>
-      <input
-        type="number"
-        name="contacto_"
-        value={nuevoCliente.contacto_}
-        onChange={manejarCambioFormulario}
-        className={styles.formInput}
-        required
-      />
-    </div>
-    <div className={styles.formGroup}>
-      <label className={styles.formLabel}>Compras:</label>
-      <input
-        type="text"
-        name="compras_"
-        value={nuevoCliente.compras_.join(',')}
-        onChange={(e) => manejarCambioFormulario(e)}
-        className={styles.formInput}
-      />
-    </div>
-    <div className={styles.formGroupCheckbox}>
-      <label className={styles.formLabel}>Membresía:</label>
-      <input
-        type="checkbox"
-        name="membresia_"
-        checked={nuevoCliente.membresia_}
-        onChange={() =>
-          setNuevoCliente((prevState) => ({
-            ...prevState,
-            membresia_: !prevState.membresia_,
-          }))
-        }
-        className={styles.formCheckbox}
-      />
-    </div>
-    <div className={styles.formButtonGroup}>
-      <button type="submit" className={styles.formButton}>
-        Crear Cliente
-      </button>
-      <button
-        type="button"
-        onClick={() => setMostrarFormulario(false)}
-        className={styles.formButtonCancel}
-      >
-        Cancelar
-      </button>
-    </div>
-  </form>
-)}
 
-            
-            {/* Contenido de la página */}
-            <div className={styles.content}>
-              <h1>Página de clientes pa administradores</h1>
-              <p>Bienvenido</p>
+        {mostrarFormularioEliminar && (
+            <form onSubmit={manejarEnvioEliminarCliente} className={styles.formularioContainer}>
+              <h2 className={styles.formTitle}>Eliminar Cliente</h2>
+              <div className={styles.formGroup}>
+                <label className={styles.formLabel}>DNI del Cliente:</label>
+                <input
+                  type="text"
+                  name="dniEliminar"
+                  value={dniEliminar}
+                  onChange={(e) => setDniEliminar(e.target.value)}
+                  className={styles.formInput}
+                  required
+                />
+              </div>
+              <div className={styles.formButtonGroup}>
+                <button type="submit" className={styles.formButton}>
+                  Eliminar Cliente
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setMostrarFormularioEliminar(false)}
+                  className={styles.formButtonCancel}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </form>
+          )}
+        {mostrarFormulario && (
+          <form onSubmit={manejarEnvioFormulario} className={styles.formularioContainer}>
+            <h2 className={styles.formTitle}>Crear Nuevo Cliente</h2>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>DNI:</label>
+              <input
+                type="text"
+                name="id_"
+                value={nuevoCliente.id_}
+                onChange={manejarCambioFormulario}
+                className={styles.formInput}
+                required
+              />
             </div>
-         {/* Tabla de clientes */}
-         <div className={styles.content}>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Nombre:</label>
+              <input
+                type="text"
+                name="nombre_"
+                value={nuevoCliente.nombre_}
+                onChange={manejarCambioFormulario}
+                className={styles.formInput}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Contacto:</label>
+              <input
+                type="number"
+                name="contacto_"
+                value={nuevoCliente.contacto_}
+                onChange={manejarCambioFormulario}
+                className={styles.formInput}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Compras:</label>
+              <input
+                type="text"
+                name="compras_"
+                value={nuevoCliente.compras_.join(',')}
+                onChange={(e) => manejarCambioFormulario(e)}
+                className={styles.formInput}
+              />
+            </div>
+            <div className={styles.formGroupCheckbox}>
+              <label className={styles.formLabel}>Membresía:</label>
+              <input
+                type="checkbox"
+                name="membresia_"
+                checked={nuevoCliente.membresia_}
+                onChange={() =>
+                  setNuevoCliente((prevState) => ({
+                    ...prevState,
+                    membresia_: !prevState.membresia_,
+                  }))
+                }
+                className={styles.formCheckbox}
+              />
+            </div>
+            <div className={styles.formButtonGroup}>
+              <button type="submit" className={styles.formButton}>
+                Crear Cliente
+              </button>
+              <button
+                type="button"
+                onClick={() => setMostrarFormulario(false)}
+                className={styles.formButtonCancel}
+              >
+                Cancelar
+              </button>
+            </div>
+          </form>
+        )}
+
+
+        {/* Contenido de la página */}
+        <div className={styles.content}>
+          <h1>Página de clientes pa administradores</h1>
+          <p>Bienvenido</p>
+        </div>
+        {/* Tabla de clientes */}
+        <div className={styles.content}>
           <h1>Lista de Clientes</h1>
           <div className={styles.tableContainer}>
             <table className={styles.styledTable}>
